cd5359d43bdfff30b2090945c14b4d155c29dc18
MichalUSER <misko.urban12@gmail.com>
2021-04-17 20:20:05 +0200

    initial

---
+++ src/viac_dni.rs
@@ -0,0 +1,32 @@
+use scraper::{Html, Selector};
+use prettytable::{Cell, Row, Table};
+
+pub fn get(document: &Html) {
+    let vsetky_dni_selector = Selector::parse(".fcast-table-tabs > ul > li > a").unwrap();
+    let den_selector = Selector::parse("span:nth-child(1)").unwrap();
+    let datum_selector = Selector::parse("span:nth-child(2)").unwrap();
+    let teplota_selector = Selector::parse("span:nth-child(3)").unwrap();
+
+    let mut first_row: Vec<Cell> = Vec::new();
+    let mut second_row: Vec<Cell> = Vec::new();
+
+    for element in document.select(&vsetky_dni_selector) {
+        let den = element.select(&den_selector).next().unwrap();
+        let datum = element.select(&datum_selector).next().unwrap();
+        let teplota = element.select(&teplota_selector).next().unwrap();
+        first_row.push(Cell::new(
+            &format!(
+                "\x1b[0;34m{} {}\x1b[0m",
+                den.text().collect::<String>().trim(),
+                datum.text().collect::<String>().trim()
+            )
+            .to_string()
+        ));
+        second_row.push(Cell::new(teplota.text().collect::<String>().trim()));
+    }
+
+    let mut table = Table::new();
+    table.add_row(Row::new(first_row));
+    table.add_row(Row::new(second_row));
+    table.printstd();
+}
---
+++ src/viac_info.rs
@@ -0,0 +1,26 @@
+use scraper::{Html, Selector};
+use prettytable::{Cell, Row, Table};
+
+pub fn get(document: &Html) {
+    let info_selector = Selector::parse(".rows-wrapper > div > div").unwrap();
+    let first_row_selector = Selector::parse(".data").unwrap();
+    let second_row_selector = Selector::parse(".describe-label").unwrap();
+
+    let mut first_row_vec: Vec<Cell> = Vec::new();
+    let mut second_row_vec: Vec<Cell> = Vec::new();
+
+    for element in document.select(&info_selector) {
+        let first_row = element.select(&first_row_selector).next().unwrap();
+        let second_row = element.select(&second_row_selector).next().unwrap();
+        let name = second_row.inner_html();
+        first_row_vec.push(Cell::new(
+            format!("\x1b[0;34m{}\x1b[0m", name).as_str()
+        ));
+        second_row_vec.push(Cell::new(first_row.text().collect::<String>().trim()));
+    }
+
+    let mut table = Table::new();
+    table.add_row(Row::new(first_row_vec));
+    table.add_row(Row::new(second_row_vec));
+    table.printstd();
+}
